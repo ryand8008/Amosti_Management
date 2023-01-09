02db4f08bcf897220432db036d247ad45144c408
Ryan De Leon <ryandeleon0@gmail.com>
2023-01-08 18:44:14 -0800

    added function to download full report to excel file

--- src/components/Report/ExportFile.tsx
+++ src/components/Report/ExportFile.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useEffect } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import { AggregateContext } from "../context/ProjectContext";
 
 var xlsx = require("xlsx");
@@ -9,29 +9,19 @@ export const ExportFile = ({report}) => {
 
   const hardCodeHeaders = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre','noviembre', 'diciembre', 'anual' ]
 
+  const sheetHeaders = ['Edificio', 'enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre','noviembre', 'diciembre', 'anual']
   const buildings = Object.keys(report).filter((item) => isNaN(Number(item)) === true)
-// example:
-// [
-//   { C: 2, D: 3 },
-//   { D: 1, C: 4 } // different key order
-// ]
 
-// should be =>
-// [
-//  {hardCodeHeaders[num]: item },
-//  {...the rest}
-// ]
-let x = 0
+  const [rows, setRows] = useState([])
+
   useEffect(() => {
-    console.log(reportInfo, 'reportInfo')
+
     if (report[yearPicked]['totalRev']) {
-      x = x + 1;
-      console.log('how many times will it render? ', x)
 
       createWorkBook()
 
     }
-  }, [JSON.stringify(report)])
+  }, [JSON.stringify(report), rows.length])
 
 
   const getTotal = (totalHeader) => {
@@ -45,7 +35,7 @@ let x = 0
           tempTotal = {'Edificio': 'Neto Total'}
         }
         report[yearPicked][totalHeader].forEach((item, index) => {
-          // console.log(item, 'there should be bunch')
+
           tempTotal = {...tempTotal, ...{[hardCodeHeaders[index]]: item}}
 
         })
@@ -69,51 +59,32 @@ let x = 0
     let totalToAdd = getTotal(totalHeader)
               tempArray.push(totalToAdd)
               tempArray.push([])
-    console.log(tempArray, 'this should be [{}, {}]')
     return tempArray;
   }
 
-  // reportInfo[buildingName][year]
-    const createWorkBook = () => {
+  const createWorkBook = () => {
+    let testRow = []
 
-      let testRow = []
       for (let i = 0; i <= 2; i++ ) {
-        testRow.push(getInfo(i))
+        testRow.push(...getInfo(i))
       }
 
-    // create rows from Report Info
-    // const rows = hardCodeHeaders.map((item) =>
-    //   item:
-    // )
+      setRows(testRow)
 
-// this is the total of the buildings
-// reportInfo[year]['totalRev']
-// reportInfo[year]['totalExpenses']
-// reportInfo[year]['totalProfit']
-
-
-    // const worksheet = xlsx.utils.json_to_sheet()
-    // const workbook = xlsx.utils.book_new();
-    // XLSX.utils.sheet_add_aoa(worksheet, [hardCodeHeaders], { origin: "A1" });
   }
 
+const exportExcel = async () => {
+    const worksheet = xlsx.utils.json_to_sheet(rows)
+    const workbook = xlsx.utils.book_new();
+    xlsx.utils.book_append_sheet(workbook, worksheet, "TESTING");
+    xlsx.utils.sheet_add_aoa(worksheet, [sheetHeaders], { origin: "A1" });
 
+    xlsx.writeFile(workbook, `FullReport${yearPicked}.xlsx`, {compression: true})
+}
 
   return (
     <>
-      <h1>Hello from ExportFile</h1>
-      <button onClick={(e) => {e.preventDefault(), alert('you clicked me')}}>EXPORT ME</button>
+      {rows.length > 0 ? <button onClick={() => {exportExcel()}}>EXPORT FILE</button> : null}
     </>
   )
 }
-
-
-// buildings.forEach((building, buildingIndex) => {
-//   let temp = { 'Edificio': building }
-
-//   report[building][yearPicked][header].forEach((item, index) => {
-//     temp = { ...temp, ...{ [hardCodeHeaders[index]]: item } }
-//   })
-
-
-//   testRow.push(temp)
