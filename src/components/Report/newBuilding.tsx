c27bfe03377bb48e21e04e1506ea59247e60cfe8
Ryan De Leon <ryandeleon0@gmail.com>
2023-02-26 19:45:18 -0800

    NewReport created, will need to test.

--- src/components/Report/newBuilding.tsx
+++ src/components/Report/newBuilding.tsx
@@ -2,10 +2,11 @@ import React from "react";
 
 interface NewBuildingProps {
   buildingName: string;
+  aggregate: any
 }
 
-export const NewBuilding = ({buildingName}: NewBuildingProps) => {
-
+export const NewBuilding = ({aggregate, buildingName}: NewBuildingProps) => {
+  console.log(aggregate, 'this is agg')
   return (
     <>
       <h1>hello from New Building</h1>
--- src/components/Report/newReport.tsx
+++ src/components/Report/newReport.tsx
@@ -1,17 +1,26 @@
-import React, { useContext } from "react";
-import { AggregateContext } from "components/context/ProjectContext";
+import React, { useContext, useEffect, useMemo } from "react";
+import { AggregateContext } from "../context/ProjectContext";
 import { NewBuilding } from "./newBuilding";
 
 export const NewReport = () => {
   const { aggregate } = useContext(AggregateContext)
-  const buildings = Object.keys(aggregate)
 
+  const buildings = useMemo(() => {
+    if (aggregate) {
+      return Object.keys(aggregate)
+    }
+    return []
+  }, [aggregate])
+
+  useEffect(() => {
+    console.log(buildings, 'useeffect buildings')
+  }, [buildings])
 
   return (
     <>
       <h1>It's me, from NewReport</h1>
       {buildings.map((building) =>
-        <NewBuilding buildingName={building} />
+        <NewBuilding aggregate={aggregate} buildingName={building} />
       )}
     </>
   )
